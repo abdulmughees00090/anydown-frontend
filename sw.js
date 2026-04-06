243ad5db3121d4d25872061979479a7294867851
Abdul Mughees <77141705+abdulmughees00090@users.noreply.github.com>
2026-04-06 17:11:20 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10837822
+    "domain": "3nbf4.com",
+    "zoneId": 10839777
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
